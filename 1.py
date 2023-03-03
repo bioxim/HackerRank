a3e727602c406d971dd83da1151ecbec4ebe406f
Xime Camino <ximecamino@gmail.com>
2023-03-02 21:54:27 -0300

    first commit

---
+++ 1.py
@@ -0,0 +1,12 @@
+# Ejercicio 1 es un hola mundo de introducción
+# Say "Hello, World!" With Python
+
+hola = "Hola Mundo!!"
+
+print(hola)
+
+nombre = "Xime"
+
+saludo = f"Hola {nombre}, cómo estás?"
+
+print(saludo)
---
+++ ALGORITMOS/1.py
@@ -0,0 +1,10 @@
+def solveMeFirst(a,b):
+	# Hint: Type return a+b below
+    return a+b
+
+
+num1 = 5
+num2 = 7
+
+res = solveMeFirst(num1,num2)
+print(res)
